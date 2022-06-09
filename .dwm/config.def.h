fa51c30c6f738f0c0d8a32a001d42d1b8c891602
aevim <aedigo12@protonmail.com>
2022-06-09 11:23:03 -0300

    new changes!

--- .dwm/config.def.h
+++ .dwm/config.def.h
@@ -71,8 +71,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-
+static const char *termcmd[]  = { "st", "-A 100", NULL };
 /*First arg only serves to match against key in rules*/
 static const char *scratchpadcmd[] = {"s", "st", "-t", "scratchpad", NULL}; 
 static const char *volup[] = {"volume.sh", "up", NULL}; 
