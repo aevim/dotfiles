c6778015d7324b99ecff334f2843daaa6605c95c
aevim <aedigo12@protonmail.com>
2022-07-04 19:38:40 -0300

    new changes!

--- .dwm/config.def.h
+++ .dwm/config.def.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display sy
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-
+static const int focusonwheel       = 0;
 static const char *fonts[]          = { "VictorMono:size=12" };
 static const char dmenufont[]       = "VictorMono:size=12";
 static const char bg[]       = "#282828";
