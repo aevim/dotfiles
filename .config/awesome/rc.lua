f3431644e21fe3dea686e36f96dd9bfae2b34e11
aevim <aedigo12@gmail.com>
2022-08-06 19:47:03 -0300

    new changes!

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -21,12 +21,12 @@ local bling = require("bling")
 
 terminal = "st"
 local term_scratch = bling.module.scratchpad {
-    command = terminal .. " -c spad",           -- How to spawn the scratchpad
+    command = terminal .. " -A 0.6 -c spad",           -- How to spawn the scratchpad
     rule = { class = "spad" },                     -- The rule that the scratchpad will be searched by
     sticky = true,                                    -- Whether the scratchpad should be sticky
     autoclose = true,                                 -- Whether it should hide itself when losing focus
-    floating = false,                                  -- Whether it should be floating (MUST BE TRUE FOR ANIMATIONS)
-    geometry = {x=0, y=90, height=100, width=1200}, -- The geometry in a floating state
+    floating = true,                                  -- Whether it should be floating (MUST BE TRUE FOR ANIMATIONS)
+    geometry = {x=70, y=20, height=300, width=1200}, -- The geometry in a floating state
     reapply = true,                                   -- Whether all those properties should be reapplied on every new opening of the scratchpad (MUST BE TRUE FOR ANIMATIONS)
     dont_focus_before_close  = false,                 -- When set to true, the scratchpad will be closed by the toggle function regardless of whether its focused or not. When set to false, the toggle function will first bring the scratchpad into focus and only close it on a second call
 }
@@ -465,6 +465,8 @@ root.keys(globalkeys)
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
+    { rule = { class = "Firefox" },
+      properties = { tag = "2" } },
     -- All clients will match this rule.
     { rule = { },
       properties = { border_width = beautiful.border_width,
