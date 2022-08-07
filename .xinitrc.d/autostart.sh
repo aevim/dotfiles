a8866996ca5c7b792e5c49fb57345a70a76cecb2
aevim <aedigo12@gmail.com>
2022-08-07 08:01:00 -0300

    new changes!

---
+++ .xinitrc.d/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+hsetroot -cover ~/.wallpapers/rainnight.jpg &
+xrandr --output VGA-1 --gamma 1.0:0.88:0.90 --brightness 0.95 &
+unclutter &
+dunst &
+safeeyes &
+xset led on &
+keyd-application-mapper -d
+run_xidlehook &
+emacs --daemon &
+pipewire &
+pipewire-pulse &
